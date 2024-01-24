5d4b8ddd0c180074355d18318608f24f94d5eee0
Carson Kompon <carsokompo@gmail.com>
2024-01-24 07:28:40 -0500

    Enemies now have an attack animation

--- code/DamageTrigger.cs
+++ code/DamageTrigger.cs
@@ -1,26 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Sandbox;
+using Sandbox.Citizen;
 
 public sealed class DamageTrigger : Component, Component.ITriggerListener
 {
 	[Property] public float Damage { get; set; } = 10f;
 	[Property] public float Cooldown { get; set; } = 1f;
+	[Property] CitizenAnimationHelper CitizenAnimator { get; set; }
 
 	[Sync] TimeSince TimeSinceLastDamage { get; set; } = 0f;
 
+	List<Player> canHurt = new();
+
+	protected override void OnUpdate()
+	{
+		if ( IsProxy ) return;
+		if ( TimeSinceLastDamage >= Cooldown && canHurt.Count > 0 )
+		{
+			foreach ( var player in canHurt )
+			{
+				if ( player is null ) continue;
+				player.Hurt( Damage );
+			}
+			TimeSinceLastDamage = 0f;
+
+			BroadcastAttackEvent();
+		}
+	}
+
 	public void OnTriggerEnter( Collider other )
 	{
-		if ( TimeSinceLastDamage < Cooldown ) return;
+		if ( IsProxy ) return;
 		if ( other.Components.GetInParentOrSelf<Player>() is Player player )
 		{
-			if ( player.IsProxy ) return;
-
-			player.Hurt( Damage );
-			TimeSinceLastDamage = 0f;
+			canHurt.Add( player );
 		}
 	}
 
 	public void OnTriggerExit( Collider other )
 	{
+		if ( IsProxy ) return;
+		if ( other.Components.GetInParentOrSelf<Player>() is Player player )
+		{
+			canHurt.Remove( player );
+		}
+	}
 
+	[Broadcast]
+	void BroadcastAttackEvent()
+	{
+		PlayAttackAnim();
+	}
+
+	async void PlayAttackAnim()
+	{
+		if ( CitizenAnimator is null ) return;
+		if ( Random.Shared.Float() < .5f )
+			CitizenAnimator.HoldType = CitizenAnimationHelper.HoldTypes.Swing;
+		else
+			CitizenAnimator.HoldType = CitizenAnimationHelper.HoldTypes.Punch;
+		CitizenAnimator.Target.Set( "b_attack", true );
+		await GameTask.DelayRealtimeSeconds( Cooldown / 2f );
+		CitizenAnimator.HoldType = CitizenAnimationHelper.HoldTypes.None;
 	}
 }
