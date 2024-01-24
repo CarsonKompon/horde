21e9890afb61a4243d4486f6a1a6399864a6f404
Carson Kompon <carsokompo@gmail.com>
2024-01-24 11:41:47 -0500

    Fixed player map spawns

--- code/MapManager.cs
+++ code/MapManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Sandbox;
 
 public sealed class MapManager : Component
@@ -23,14 +25,39 @@ public sealed class MapManager : Component
 	{
 		Scene.Title = Map.MapName;
 
+		bool createdSpawns = false;
+
 		foreach ( var obj in Map.GameObject.Children )
 		{
 
-			if ( obj.Name == "sd_weaponspawn_random" )
+			if ( obj.Name == "sd_weaponspawn_random" || obj.Name.StartsWith( "tf_healthkit" ) || obj.Name.StartsWith( "tf_ammopack" ) )
 			{
 				var weaponSpawner = EnemySpawnerPrefab.Clone( obj.Transform.Position );
 				weaponSpawner.NetworkSpawn( null );
+				createdSpawns = true;
 			}
+		}
+
+		if ( !createdSpawns )
+		{
+			foreach ( var obj in Map.GameObject.Children )
+			{
+
+				if ( obj.Name == "info_player_start" )
+				{
+					var weaponSpawner = EnemySpawnerPrefab.Clone( obj.Transform.Position );
+					weaponSpawner.NetworkSpawn( null );
+				}
+			}
+		}
+
+		var spawnPoints = Scene.Components.GetAll<SpawnPoint>().OrderBy( x => new Guid() ).ToList();
+		var spawnIndex = 0;
+		foreach ( var player in Scene.Components.GetAll<Player>() )
+		{
+			player.CharacterController.Velocity = Vector3.Zero;
+			player.Transform.Position = spawnPoints[spawnIndex].Transform.Position + Vector3.Up * 4f;
+			spawnIndex = (spawnIndex + 1) % spawnPoints.Count;
 		}
 	}
 }
