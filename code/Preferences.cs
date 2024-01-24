f5ff19a892f75631263ee68a9516437319d5bfbc
Carson Kompon <carsokompo@gmail.com>
2024-01-24 14:13:37 -0500

    Added a Chatbox

--- code/Preferences.cs
+++ code/Preferences.cs
@@ -17,6 +17,20 @@ public static class HordePreferences
     }
     static HordeSettings _settings;
 
+    public static ChatSettings Chat
+    {
+        get
+        {
+            if ( _chatSettings is null )
+            {
+                var file = "/settings/chat.json";
+                _chatSettings = FileSystem.Data.ReadJson( file, new ChatSettings() );
+            }
+            return _chatSettings;
+        }
+    }
+    static ChatSettings _chatSettings;
+
     public static void Save()
     {
         var file = "/settings.json";
@@ -28,4 +42,11 @@ public static class HordePreferences
 public class HordeSettings
 {
     public float Volume { get; set; } = 40f;
+}
+
+public class ChatSettings
+{
+    public bool ShowAvatars { get; set; } = true;
+    public int FontSize { get; set; } = 16;
+    public bool ChatSounds { get; set; } = true;
 }
